dd0e55552c1cc5579ea12d06a18b94eeffe785e0
Sahithi <nandyalasahithi16@gmail.com>
2018-11-21 07:39:26 -0800

    BT from inorder and preorder

---
+++ Programming/C/preinBT.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int 
+	printf("Enter the inorder traversal of your BT\n");
+	
+	printf("Enter the preorder traversal of your BT\n");
+
+
+	return 0;
+}
